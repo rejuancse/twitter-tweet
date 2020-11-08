3049e9f6ccac76722c33b0e799801d9a96e22964
Rejuan Ahamed <rejuancse@gmail.com>
2020-11-08 19:57:16 +0600

    Name script add

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="app">
+        @{{user.username}} - {{fullname}}
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        data() {
+            return {
+                followers: 0,
+                user: {
+                    id: 1,
+                    username: '_rejuancse',
+                    firstName: 'Rejuan',
+                    lastName: 'Ahamed',
+                    email: 'rejuancse@gmail.com',
+                    isAdmin: true
+                }
+            }
+        },
+        computed: {
+            fullname() {
+                return `${this.user.firstName}`;
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
